d78bdd87a2fcb60467153fcfc26f37549fcb1ed9
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-29 23:38:03 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,11 @@
+let newThing = [];
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-  return i === 1 ? `I am 1 strange loop.` : `I am ${i} strange loops.`
+    if (array[i] === 1) {
+      newThing.push(`I am 1 strange loop.`);
+    } else {
+      newThing.push(`I am ${i} strange loops.`);
+    }
+  // return i === 1 ? `I am 1 strange loop.` : `I am ${i} strange loops.`
 }
 }
